1b888bd3ab5ac28c9b4c602164745c5e092a963b
Joel Storr <joel.storr@ewe.net>
2023-06-01 21:36:34 +0200

    Added Done List Item Display

--- lib/widgets/done_totos_overlay.dart
+++ lib/widgets/done_totos_overlay.dart
@@ -12,11 +12,22 @@ class DoneTodosOverlay extends StatelessWidget {
       decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.background.withOpacity(0.8),
           borderRadius: BorderRadius.circular(25)),
-      padding: const EdgeInsets.all(30),
+      padding: const EdgeInsets.only(top: 30),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           const Text('See what you accomplished'),
+          TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.arrow_downward,
+                color: Colors.white,
+                size: 15,
+              ),
+              label: const Text(
+                'Swipe down to close',
+                style: TextStyle(color: Colors.white, fontSize: 12),
+              )),
           const SizedBox(
             height: 30,
           ),
@@ -28,10 +39,18 @@ class DoneTodosOverlay extends StatelessWidget {
               return ListTile(
                 key: Key(index.toString()),
                 leading: IconButton(
-                  icon: const Icon(Icons.check),
+                  icon: const Icon(
+                    Icons.check,
+                    color: Colors.green,
+                  ),
                   onPressed: () {},
                 ),
-                title: Text(doneTodos[index]!),
+                title: Text(
+                  doneTodos[index]!,
+                  style: TextStyle(
+                      color: Colors.green,
+                      decoration: TextDecoration.lineThrough),
+                ),
               );
             },
           ),
