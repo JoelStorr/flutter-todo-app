ec3479212cccb567e2cddfcbd666f63fade6304a
Joel Storr <joel.storr@ewe.net>
2023-06-02 22:14:55 +0200

    Added  Todo Item Class

--- lib/models/todo_item_model.dart
+++ lib/models/todo_item_model.dart
@@ -3,12 +3,16 @@ import 'package:uuid/uuid.dart';
 const uuid = Uuid();
 
 class TodoItem {
-  TodoItem({required this.name, time, this.done = false})
-      : id = uuid.v1(),
+  TodoItem({
+    required this.name,
+    required this.parentId,
+    time,
+    this.done = false,
+  })  : id = uuid.v1(),
         time = time ?? DateTime.now();
 
   final String id;
-
+  final String parentId;
   String name;
   DateTime time;
   bool done;
