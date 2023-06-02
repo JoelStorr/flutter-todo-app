c02342cde1587d362f4830badae5a6bd541fc81e
Joel Storr <joel.storr@ewe.net>
2023-06-02 14:58:15 +0200

    Added Add Todolist Functionality

--- lib/widgets/side_drawer.dart
+++ lib/widgets/side_drawer.dart
@@ -10,50 +10,94 @@ class MySideDrawer extends StatefulWidget {
 class _MySideDrawerState extends State<MySideDrawer> {
   final List todoList = ['Work', 'Shopping', 'Sport'];
 
+  final _listTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(children: [
-        Expanded(
-          flex: 1,
-          child: Container(
-            width: MediaQuery.of(context).size.width * 0.85,
-            child: DrawerHeader(
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.black12,
-                    const Color.fromARGB(255, 167, 167, 167).withOpacity(0.3)
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
+      backgroundColor: Color.fromARGB(255, 48, 48, 48),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.85,
+              child: DrawerHeader(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.black12,
+                      const Color.fromARGB(255, 167, 167, 167).withOpacity(0.3)
+                    ],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
                 ),
-              ),
-              child: const Text(
-                'Flutter Do',
-                style: TextStyle(fontSize: 25),
+                child: const Text(
+                  'Flutter Do',
+                  style: TextStyle(fontSize: 25),
+                ),
               ),
             ),
           ),
-        ),
-        Expanded(
-          flex: 5,
-          child: ListView.builder(
-            itemCount: todoList.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: const Icon(
-                  Icons.list,
-                  size: 26,
-                  color: Colors.white70,
-                ),
-                title: Text(todoList[index]),
-              );
-            },
+          Expanded(
+            flex: 4,
+            child: ListView.builder(
+              itemCount: todoList.length,
+              itemBuilder: (context, index) {
+                if (todoList[index] != null) {
+                  return ListTile(
+                    leading: const Icon(
+                      Icons.list,
+                      size: 26,
+                      color: Colors.white70,
+                    ),
+                    title: Text(todoList[index]),
+                  );
+                } else {
+                  return ListTile(
+                    leading: const Icon(Icons.edit),
+                    title: TextField(
+                      controller: _listTextController,
+                      onSubmitted: (String value) {
+                        setState(() {
+                          if (value.trim().isEmpty) {
+                            todoList.remove(null);
+                            _listTextController.clear();
+                            return;
+                          }
+                          todoList[index] = value;
+                          _listTextController.clear();
+                        });
+                      },
+                      autofocus: true,
+                    ),
+                  );
+                }
+              },
+            ),
           ),
-        ),
-      ]),
+          SafeArea(
+            child: TextButton.icon(
+              onPressed: () {
+                setState(() {
+                  todoList.add(null);
+                });
+              },
+              icon: const Icon(
+                Icons.add,
+                color: Color.fromARGB(255, 134, 177, 198),
+              ),
+              label: const Text(
+                'Add new List',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 134, 177, 198),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
